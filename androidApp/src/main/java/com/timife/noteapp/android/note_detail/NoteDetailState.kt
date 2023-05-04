9d871b65ddcdc3bcd1e6c6581f23ada443d3a9ac
Timothy Ademola <timife007@gmail.com>
2023-05-04 15:05:47 +0100

    note detail viewmodel

--- androidApp/src/main/java/com/timife/noteapp/android/note_detail/NoteDetailState.kt
+++ androidApp/src/main/java/com/timife/noteapp/android/note_detail/NoteDetailState.kt
@@ -1,11 +1,9 @@
 package com.timife.noteapp.android.note_detail
 
-import androidx.compose.ui.graphics.Color
-
 data class NoteDetailState(
     val noteTitle:String = "",
-    val isNoteTitleTextFocused:Boolean = false,
+    val isNoteTitleHintVisible:Boolean = false,
     val noteContent:String = "",
-    val isNoteContentTextFocused:Boolean = false,
-    val noteColor:Color = Color.White
+    val isNoteContentHintVisible:Boolean = false,
+    val noteColor: Long = 0xFFFFFF
 )
